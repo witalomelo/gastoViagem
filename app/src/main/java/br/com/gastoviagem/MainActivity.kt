e8387243b20f6815a764af04a19117c47e167d7f
witalomelo <witalovmelo@gmail.com>
2024-05-16 16:20:54 -0300

    logica do calculo

--- app/src/main/java/br/com/gastoviagem/MainActivity.kt
+++ app/src/main/java/br/com/gastoviagem/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import br.com.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // View Binding
-        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.buttonCalculate.setOnClickListener(this)
@@ -26,8 +28,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun calculate() {
-        //Toats notification
-        Toast.makeText(this, "Clicado", Toast.LENGTH_SHORT).show()
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.textPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+        binding.textViewTotalValue.text =  "R$ ${"%.2f".format(totalValue)}"
+
+        // Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
 
     }
 
