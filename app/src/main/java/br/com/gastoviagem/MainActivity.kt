05d26e09c608e6c41816e02018d56d09bd1fefcb
witalomelo <witalovmelo@gmail.com>
2024-05-16 07:49:38 -0300

    evento de clique

--- app/src/main/java/br/com/gastoviagem/MainActivity.kt
+++ app/src/main/java/br/com/gastoviagem/MainActivity.kt
@@ -1,11 +1,12 @@
 package br.com.gastoviagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import br.com.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +14,21 @@ class MainActivity : AppCompatActivity() {
         // View Binding
         var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+
+        }
+    }
+
+    private fun calculate() {
+        //Toats notification
+        Toast.makeText(this, "Clicado", Toast.LENGTH_SHORT).show()
+
     }
 
 
