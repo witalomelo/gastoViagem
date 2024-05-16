d154b8f01a85f42b4bd6ac35ce1d85d1c1a6407a
witalomelo <witalovmelo@gmail.com>
2024-05-16 07:36:53 -0300

    construcao do layout

--- app/src/main/java/br/com/gastoviagem/MainActivity.kt
+++ app/src/main/java/br/com/gastoviagem/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import br.com.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // View Binding
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
